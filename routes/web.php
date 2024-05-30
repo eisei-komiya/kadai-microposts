02953bac92a2730578d34527eb87465e5fd3c322
eisei.komiya <eisei-komiya@dac.co.jp>
2024-05-30 06:29:16 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 //use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController; // 追記
+use App\Http\Controllers\UsersController; 
+use App\Http\Controllers\MicropostsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,19 +15,17 @@ use App\Http\Controllers\UsersController; // 追記
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [MicropostsController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');//認証済みの人のみ通過
+Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
 
 require __DIR__.'/auth.php';
